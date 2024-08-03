e32ef85fd4590c259fbf2e38a648a4a9ab374b06
yavuzmutlu <yavuzmutlu41@gmail.com>
2024-08-03 08:13:18 +0300

    Ders notları

---
+++ app/src/main/java/com/example/yasamdongusu/SecondActivity.kt
@@ -0,0 +1,39 @@
+package com.example.yasamdongusu
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.yasamdongusu.databinding.ActivityMainBinding
+import com.example.yasamdongusu.databinding.ActivitySecondBinding
+
+class SecondActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySecondBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        enableEdgeToEdge()
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val maindenGelenIntent = intent
+        val yollananIsim = maindenGelenIntent.getStringExtra("isim")
+
+
+        binding.textView2.text = yollananIsim
+
+    }
+
+}
